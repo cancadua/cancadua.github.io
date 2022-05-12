579470593d97a3c3f2b3f27b1c38b76ef3cb2c24
cancadua <33159@student.pwsztar.edu.pl>
2022-05-12 11:40:43 +0200

    Start

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className={'navbar'}>
+            <span className={'navbar-home'}>ME</span>
+        </div>
+    )
+}
+
+export default Navbar;
